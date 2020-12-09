c3fceeada3c90df7bc7ee1e9409d4f284bb62ce3
Restyled.io <commits@restyled.io>
2020-12-09 14:59:05 +0000

    Restyled by prettier

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const fetch = require("node-fetch")
+const express = require("express");
+const app = express();
+const port = 3000;
+const fetch = require("node-fetch");
 app.get(/(.*)/, (req, res) => {
-  let website = req.path.substring(1,req.path.length)
-console.log(website)
-  fetch(website).then(res=>res.json()).then(json=>{res.json(json)}).catch(e=>{res.status("400").send(e)})
-})
+  let website = req.path.substring(1, req.path.length);
+  console.log(website);
+  fetch(website)
+    .then((res) => res.json())
+    .then((json) => {
+      res.json(json);
+    })
+    .catch((e) => {
+      res.status("400").send(e);
+    });
+});
 
-  app.listen(port, () => {console.log(
-                       `Example app listening at http://localhost:${port}`)})
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
