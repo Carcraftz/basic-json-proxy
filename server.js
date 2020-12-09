565f2c97045c2f83a22451e6bd273e6082f1c6fe
Restyled.io <commits@restyled.io>
2020-12-09 14:59:03 +0000

    Restyled by clang-format

--- server.js
+++ server.js
@@ -4,10 +4,9 @@ const port = 3000
 const fetch = require("node-fetch")
 app.get(/(.*)/, (req, res) => {
   let website = req.path.substring(1,req.path.length)
-  console.log(website)
+console.log(website)
   fetch(website).then(res=>res.json()).then(json=>{res.json(json)}).catch(e=>{res.status("400").send(e)})
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  app.listen(port, () => {console.log(
+                       `Example app listening at http://localhost:${port}`)})
