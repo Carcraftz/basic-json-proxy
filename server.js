0c08668b123e2a189c875ac019db5d20c97460f5
Carcraftz <40304589+Carcraftz@users.noreply.github.com>
2021-04-07 00:55:39 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fetch = require("node-fetch")
 app.get(/(.*)/, (req, res) => {
   let website = req.path.substring(1,req.path.length)
   console.log(website)
-  fetch(website).then(res=>res.json()).then(json=>{res.json(json)}).catch(e=>{res.status("400").send(e)})
+  fetch(website,{headers:{Referer:"https://www.cvs.com/immunizations/covid-19-vaccine"}}).then(res=>res.json()).then(json=>{res.json(json)}).catch(e=>{res.status("400").send(e)})
 })
 
 app.listen(process.env.PORT || port, () => {
